aaba6165a678675739d02bdb6f9d2bd4214e7ef5
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-06-03 17:15:57 +0900

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,17 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'go-to-k',
+  authorAddress: '24818752+go-to-k@users.noreply.github.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  jsiiVersion: '~5.0.0',
+  name: 'elb-other-5xx-alarm',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/24818752+go-to-k/elb-other-5xx-alarm.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
